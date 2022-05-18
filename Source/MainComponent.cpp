b59f7e1b08f82b385acf9cf8cc91387a3eb3fc92
Adhémar de Senneville <adhemar2senneville@gmail.com>
2022-05-18 20:50:28 +0200

    Change Title

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -18,7 +18,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::Font (48.0f));
     g.setColour (juce::Colours::red);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Ici c'est graphic", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
